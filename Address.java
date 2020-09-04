78fe8a2c403c7881c05ddbb8ae179f4e172f4944
ok-qa <69260710+ok-qa@users.noreply.github.com>
2020-09-04 13:28:40 -0700

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Address {
+    public static void address(String[] args)  {
+
+        //Номер дома
+        long houseNumber;
+        //Улица
+        String street;
+        //Город
+        String city;
+        //Штат
+        String state;
+        //Индекс
+        Long zipCode;
+
+    }
+
+}
---
+++ Contact.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private long phoneNumber;
+    private Address address;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+}
